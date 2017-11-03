1bf84918ed7fb58a5f59212cd5fb3d25b1e154dd
Laura Travin <laura.travin@gmail.com>
2017-11-03 23:05:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,13 +10,15 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 // The destructive function, destructivelyAddElementToBeginningOfArray, should alter the original
 //array that's passed in; addElementToBeginningOfArray, on the other hand, should return a new array
 //and not modify the original.
-// var test = new Array();
-// function addElementToBeginningOfArray(test,'1') {
-//        test.push = ['1'];
-//
-// }
-//
-// function destructivelyAddElementToBeginningOfArray(){}
+
+
+var test = new Array();
+function addElementToBeginningOfArray(test,'1') {
+       test.push = ['1'];
+
+}
+
+function destructivelyAddElementToBeginningOfArray(){}
 
 //These functions also take two arguments, an array and an element to add to the
 // end of the array. addElementToEndOfArray should not alter the original array;
