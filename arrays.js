f1b8dee2acf4fa99e9c0ca581c296c155a239cfd
Laura Travin <laura.travin@gmail.com>
2017-11-03 23:29:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,9 +13,9 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 
-function addElementToBeginningOfArray(arrayOfThings,value) {
-
-       return arrayOfThings.unshift(value);
+function addElementToBeginningOfArray(arrayOfThings, value) {
+      arrayOfThings.unshift(value);
+       return arrayOfThings;
 }
 
 function destructivelyAddElementToBeginningOfArray(){}
